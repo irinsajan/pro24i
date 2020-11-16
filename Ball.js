96be44f211f3475b758b567523b222c1450ca997
irinsajan <69659076+irinsajan@users.noreply.github.com>
2020-11-16 17:44:11 +0530

    Update Ball.js

--- Ball.js
+++ Ball.js
@@ -1,6 +1,6 @@
 class Ball{
     constructor(x,y,radius){
-        this.body = Matter.Bodies.circle(x,y,radius,{restitution:0.8})
+        this.body = Matter.Bodies.circle(x,y,radius,{restitution:0.3, friction: 0.5, density:1.2});
         World.add(world,this.body);
         this.radius = radius;
     }
